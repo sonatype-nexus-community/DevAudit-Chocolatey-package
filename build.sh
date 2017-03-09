9f01aff7f54e26d2d87f8a8a1225d29e2726b6ca
Ken Duck <ken@qracorp.com>
2017-03-08 23:03:23 -0500

    Make script get patch number from build

--- build.sh
+++ build.sh
@@ -10,8 +10,9 @@ mkdir work
 mkdir work/tools
 
 MAXBUILD=0
+MAXPATCH=0
 
-for FILE in `find ../DevAudit.Windows-2.0.x/DevAudit-2.0.2.*.zip -type f`
+for FILE in `find ../DevAudit.Windows-2.0.x/DevAudit-2.0.*.*.zip -type f`
 do
 	PATH=(${FILE//\// })
 	FNAME=${PATH[2]}
@@ -22,15 +23,18 @@ do
 	MINOR=${VERSION[1]}
 	PATCH=${VERSION[2]}
 	BUILD=${VERSION[3]}
-	
+	if [ $PATCH -gt $MAXPATCH ]
+	then
+		MAXPATCH=$PATCH
+	fi
 	if [ $BUILD -gt $MAXBUILD ]
 	then
 		MAXBUILD=$BUILD
 	fi
 done
 
-echo "VERSION: $MAJOR.$MINOR.$PATCH.$MAXBUILD"
-MD5=`/usr/bin/md5sum ../DevAudit.Windows-2.0.x/DevAudit-$MAJOR.$MINOR.$PATCH.$MAXBUILD.zip | /usr/bin/gawk '{print $1}'`
+echo "VERSION: $MAJOR.$MINOR.$MAXPATCH.$MAXBUILD"
+MD5=`/usr/bin/md5sum ../DevAudit.Windows-2.0.x/DevAudit-$MAJOR.$MINOR.$MAXPATCH.$MAXBUILD.zip | /usr/bin/gawk '{print $1}'`
  
 /usr/bin/sed -e "s/{{VERSION}}/$MAJOR.$MINOR.$PATCH.$MAXBUILD/" devaudit.nuspec > work/devaudit.nuspec
 /usr/bin/sed -e "s/{{VERSION}}/$MAJOR.$MINOR.$PATCH.$MAXBUILD/g" -e "s/{{MD5}}/$MD5/g" -e 's/#.*$//' -e '/^[[:space:]]*$/d' tools/chocolateyinstall.ps1 > work/tools/chocolateyinstall.ps1
